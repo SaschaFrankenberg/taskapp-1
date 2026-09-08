9047c4c138d65044d60464d63dfeb0917ae10d65
SaschaFrankenberg <saschafrankenberg90@gmail.com>
2026-09-08 08:21:48 +0200

    kleine Ergänzunh

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,7 +10,7 @@ class TaskController extends Controller
     public function index()
     {
         $tasks = Task::where('done', true)->latest()->paginate(10);
-        return view('tasks.index', ['tasks' => $tasks]); //pfadstrukturen mit . nicht mit /
+        return view('tasks.index', compact('tasks')); //pfadstrukturen mit . nicht mit /
     }
 
     public function show(Task $task)
